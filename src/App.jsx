c4e7c3a32e1f82fd5e8d1ff84b0e3d2468de8174
fred-rx <pignolfred.85@gmail.com>
2024-04-02 13:30:43 +0200

    Add wild restaurant

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,3 @@
-<<<<<<< Updated upstream
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-=======
 import React from 'react';
 import './App.css';
 import MenuList from './components/MenuList';
@@ -41,36 +32,9 @@ const foodItems = [
     isFavorite: false
   }
 ];
->>>>>>> Stashed changes
 
 const App = () => {
   return (
-<<<<<<< Updated upstream
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-=======
     <article>
       <h1>Wild Restaurant Menu</h1>
       {/* pass the variable foodItems as props to MenuList component */}
@@ -78,6 +42,5 @@ const App = () => {
     </article>
   );
 };
->>>>>>> Stashed changes
 
 export default App;
